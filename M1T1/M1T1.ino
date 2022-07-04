f07b0ff500b4fd07685b0eb1922dcb8c42552ea9
hockinga <80208244+hockinga@users.noreply.github.com>
2022-07-04 21:11:57 +1000

    Add files via upload

--- M1T1/M1T1.ino
+++ M1T1/M1T1.ino
@@ -2,7 +2,10 @@
 //
 void setup()
 {
+  // Log output
   Serial.begin(9600);
+
+  // Setup pins
   pinMode(2, OUTPUT);
   pinMode(3, INPUT);
 }
@@ -10,8 +13,13 @@ void setup()
 void loop()
 {
   int motion = digitalRead(3);
+
+  // Detected motion
   if (motion == HIGH)
   {
+    Serial.println("Detected motion");
+
+    // Activate LED for 0.5 seconds
     digitalWrite(2, HIGH);
     delay(500);
     digitalWrite(2, LOW);
